42022504801ee708dc0ed7305785c516f38da3fc
liuguilin <1401097349@qq.com>
2020-05-02 23:03:11 +0800

    提交配置文件

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: [
+    "@vue/app",
+    [
+      "@vue/babel-preset-jsx",
+      {
+        injectH: false
+      }
+    ]
+  ],
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "ant-design-vue",
+        libraryDirectory: "es",
+        style: true
+      }
+    ] // `style: true` 会加载 less 文件
+  ]
 };
